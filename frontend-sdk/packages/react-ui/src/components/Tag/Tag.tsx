6dc040c5975c07b73a8ccb3c78a705e334e1453b
ankit-thesys <i-ankit@thesys.dev>
2025-01-21 16:40:33 +0530

    tag and tagblock with style

--- frontend-sdk/packages/react-ui/src/components/Tag/Tag.tsx
+++ frontend-sdk/packages/react-ui/src/components/Tag/Tag.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import React from "react";
+import React, { forwardRef } from "react";
 
 export interface TagProps {
   className?: string;
@@ -8,13 +8,13 @@ export interface TagProps {
   text: React.ReactNode;
 }
 
-export const Tag = (props: TagProps) => {
+export const Tag = forwardRef<HTMLDivElement, TagProps>((props, ref) => {
   return (
-    <div className={clsx("crayon-tag", props.className)} style={props.styles}>
+    <div ref={ref} className={clsx("crayon-tag", props.className)} style={props.styles}>
       {props.icon && <span className="crayon-tag-icon">{props.icon}</span>}
       <span className="crayon-tag-text">{props.text}</span>
     </div>
   );
-};
+});
 
 Tag.displayName = "Tag";
---
+++ frontend-sdk/packages/react-ui/src/components/TagBlock/TagBlock.tsx
@@ -0,0 +1,24 @@
+import React, { forwardRef } from "react";
+import { Tag } from "../Tag";
+import clsx from "clsx";
+
+export interface TagBlockProps {
+  children: React.ReactElement<typeof Tag> | React.ReactElement<typeof Tag>[];
+  styles?: React.CSSProperties;
+  className?: string;
+}
+
+export const TagBlock = forwardRef<HTMLDivElement, TagBlockProps>((props, ref) => {
+  return (
+    <div
+      ref={ref}
+      className={clsx("crayon-tag-block", props.className)}
+      style={props.styles}
+    >
+      {props.children}
+    </div>
+  );
+});
+
+TagBlock.displayName = 'TagBlock';
+
