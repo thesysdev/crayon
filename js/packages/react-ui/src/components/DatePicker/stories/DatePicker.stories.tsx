94ce9084f383ae2e4d584d62c487110e56eb4f42
ankit-thesys <i-ankit@thesys.dev>
2025-02-06 15:39:55 +0530

    date picker fix

--- js/packages/react-ui/src/components/DatePicker/stories/DatePicker.stories.tsx
+++ js/packages/react-ui/src/components/DatePicker/stories/DatePicker.stories.tsx
@@ -230,6 +230,8 @@ export const ControlledRange: Story = {
     docs: {
       source: {
         code: `
+import { DateRange } from "react-day-picker";
+
 const ControlledRangePicker = () => {
   const [dateRange, setDateRange] = useState<DateRange>();
 
