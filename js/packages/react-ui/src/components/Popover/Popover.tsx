69d2d4d85d5386ffc8a0c6b922a88a8c5f142994
ankit-thesys <i-ankit@thesys.dev>
2025-02-16 21:43:44 +0530

    popover component, ita a utoility compomponet

--- js/packages/react-ui/src/components/Popover/Popover.tsx
+++ js/packages/react-ui/src/components/Popover/Popover.tsx
@@ -1,12 +1,19 @@
 import * as PopoverPrimitive from "@radix-ui/react-popover";
 import clsx from "clsx";
 import React from "react";
+
 const Popover = PopoverPrimitive.Root;
 
 const PopoverTrigger = PopoverPrimitive.Trigger;
 
+const PopoverClose = PopoverPrimitive.Close;
+
+const PopoverArrow = PopoverPrimitive.Arrow;
+
+const PopoverAnchor = PopoverPrimitive.Anchor;
+
 const PopoverContent = React.forwardRef<
-  React.ElementRef<typeof PopoverPrimitive.Content>,
+  React.ComponentRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 >(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
   <PopoverPrimitive.Portal>
@@ -14,12 +21,11 @@ const PopoverContent = React.forwardRef<
       ref={ref}
       align={align}
       sideOffset={sideOffset}
-      className={clsx(
-        
-        className,
-      )}
+      className={clsx("popover-content", className)}
       {...props}
     />
   </PopoverPrimitive.Portal>
 ));
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
+
+export { Popover, PopoverAnchor, PopoverArrow, PopoverClose, PopoverContent, PopoverTrigger };
