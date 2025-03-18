c94fd61ed299aee4e8d17c22b40e8b22f5349263
Hrijul Bhatnagar <hrijul@thesys.dev>
2025-03-18 10:33:32 +0530

    feat(textSchema): improve text response schem
    Fix an issue where description in TextResponseSchema was in the incorrect location

--- js/packages/stream/src/textSchema.ts
+++ js/packages/stream/src/textSchema.ts
@@ -4,8 +4,7 @@
 export const TextResponseSchema = {
   type: "object",
   properties: {
-    type: { const: "text" },
-    description: "Use this as a fallback when no other response template is applicable",
+    type: { const: "text", description: "Use this as a fallback when no other response template is applicable" },
     text: {
       type: "string",
       description: "plaintext message to be displayed to the user",
